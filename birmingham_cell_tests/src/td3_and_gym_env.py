38225e0b9f7862ee26bc337dc48dbf92d218f683
MichiDelle <m.delledonne002@unibs.it>
2024-03-08 17:41:23 +0000

    Added saving methods

--- birmingham_cell_tests/src/td3_and_gym_env.py
+++ birmingham_cell_tests/src/td3_and_gym_env.py
@@ -5,58 +5,72 @@ import numpy as np
 import birmingham_envs
 
 from stable_baselines3 import TD3
-from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+# from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.callbacks import CheckpointCallback
+from stable_baselines3.common.env_util import make_vec_env
+
 import rospkg
 import os
 
-epoch_number = 1
-max_epoch_steps = 30
-learning_start_steps = 20
+# from gymnasium import NormalizeObservation
+# from stable_baselines3.common.vec_env import VecNormalize
+
+
+test_name = "td3"
+epoch_number = 500
+max_epoch_steps = 60
+learning_start_steps = 2
 train_freq = 1
 start_epoch_number = 0
 learning_rate = 0.001
-total_epoch = 500
-model_name = 'td3_new_model' + str(max_epoch_steps) + '_' + str(learning_rate)
-data_name = 'td3_new_tests' + str(max_epoch_steps) + '_' + str(learning_rate)
-
+gamma = 0.99
 total_timesteps = max_epoch_steps * epoch_number
+model_save_freq = 15
+
+rospack = rospkg.RosPack()
+path = rospack.get_path('birmingham_cell_tests')
+
+model_repo_path = path + '/model'
+data_repo_path = path + '/data'
+log_repo_path = path + '/log'
+models_name = test_name + '_models_' + str(gamma) + '_' + str(learning_rate)
+model_name = test_name + '_model_' + str(gamma) + '_' + str(learning_rate)
+data_name = test_name + '_data_' + str(gamma) + '_' + str(learning_rate)
+log_name = test_name + '_log_' + str(gamma) + '_' + str(learning_rate)
+model_path = model_repo_path + '/' + model_name
+data_path = data_repo_path + '/' + data_name
+log_path = log_repo_path + '/' + log_name
+models_repo_path = model_repo_path + '/' + models_name
 
 env = gym.make('ConnectionEnv-v0',
                action_type='increment_value', 
                max_episode_steps=max_epoch_steps, 
                data_file_name=data_name,
                start_epoch_number=start_epoch_number,
-               debug_mode=True)
-            #    debug_mode=False)
+            #    debug_mode=True)
+               debug_mode=False)
 
-n_actions = env.action_space.shape[-1]
-action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+# env = gym.NormalizeObservation(env)
+
+checkpoint_callback = CheckpointCallback(
+    save_freq=model_save_freq,
+    save_path=models_repo_path + '/',
+    name_prefix=model_name,
+)
+
+# n_actions = env.action_space.shape[-1]
+# action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
 model = TD3("MlpPolicy", 
             env, 
             verbose=1, 
-            action_noise=action_noise,
+            # action_noise=action_noise,
+            learning_rate=learning_rate,
             learning_starts=learning_start_steps, 
-            train_freq=train_freq)
+            tensorboard_log=log_path,
+            train_freq=train_freq,
+            gamma=gamma)
 
-rospack = rospkg.RosPack()
-path = rospack.get_path('birmingham_cell_tests')
-file_path = path + "/data/" + model_name
-if os.path.isfile(file_path):
-    model.load(file_path)
-else:
-    if not (start_epoch_number == 0):
-        print('There is a problem, you set start_epoch_number equal to ' + str(start_epoch_number) + ' but the model file does not exist')
-    else:
-        print('epoch 1')
-        model.learn(total_timesteps=total_timesteps, log_interval=1)
-        model.save(path + "/data/" + model_name)
-        model.learning_start_steps = 0 
+model.learn(total_timesteps=total_timesteps, log_interval=1, callback=checkpoint_callback)
 
-for i in range(start_epoch_number, total_epoch):
-    if (start_epoch_number == 0):
-        print('epoch ' + str(i+2))
-    else:
-        print('epoch ' + str(i+1))
-    model.learn(total_timesteps=total_timesteps, log_interval=1)
-    model.save(path + "/data/" + model_name)
+model.save(model_path)
