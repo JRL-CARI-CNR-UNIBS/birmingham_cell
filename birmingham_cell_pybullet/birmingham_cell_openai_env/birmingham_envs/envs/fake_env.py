90e147cc419b8aae788a88fb54c7900b4039b41f
MichiDelle <m.delledonne002@unibs.it>
2024-03-13 11:51:45 +0000

    First tests with fake env

--- birmingham_cell_pybullet/birmingham_cell_openai_env/birmingham_envs/envs/fake_env.py
+++ birmingham_cell_pybullet/birmingham_cell_openai_env/birmingham_envs/envs/fake_env.py
@@ -4,22 +4,22 @@ from typing import Any, Dict, Optional, Tuple
 
 import rospkg
 import rospy
-import copy
+# import copy
 import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 # from gymnasium.utils import seeding
-import pyexcel_ods3 as od
+# import pyexcel_ods3 as od
 
-import tf
+# import tf
 
-from skills_util_msgs.srv import RunTree
-from pybullet_simulation.srv import SpawnModel
-from pybullet_simulation.srv import DeleteModel
-from pybullet_simulation.srv import SaveState
-from pybullet_simulation.srv import RestoreState
-from pybullet_simulation.srv import DeleteState
-from geometry_msgs.msg import Pose
+# from skills_util_msgs.srv import RunTree
+# from pybullet_simulation.srv import SpawnModel
+# from pybullet_simulation.srv import DeleteModel
+# from pybullet_simulation.srv import SaveState
+# from pybullet_simulation.srv import RestoreState
+# from pybullet_simulation.srv import DeleteState
+# from geometry_msgs.msg import Pose
 
 
 class FakeEnv(gym.Env):
@@ -82,7 +82,7 @@ class FakeEnv(gym.Env):
         self.all_param_names = []
         self.param_history = []
         self.max_variations = []
-              # Definisco la zona in cui possono essere posizionati gli oggetti di scena
+        # Definisco la zona in cui possono essere posizionati gli oggetti di scena
         self.work_space_range_low  = np.array([0.3, -0.4, 0])
         self.work_space_range_high = np.array([0.6,  0.4, 0])
         self.obj_to_grasp_pos_default = np.array([0, 0, 0.06])
@@ -119,21 +119,6 @@ class FakeEnv(gym.Env):
               ) -> Tuple[Dict[str, np.array], Dict[str, Any]]:
         super().reset(seed=seed, options=options)
         
-        # salvo i dati ottenuti dal'epoca precedente 
-        if self.param_history:
-            self.epoch_number += 1
-            data = []
-            data.append(self.all_param_names + ['reward','iteration'])
-            for index in range(len(self.param_history)):
-                data.append(self.param_history[index])
-            param_history_ods = od.get_data(self.package_path + "/data/" + self.data_file_name)
-            param_history_ods.update({str(self.epoch_number): data})
-            od.save_data(self.package_path + "/data/" + self.data_file_name, param_history_ods)
-            self.param_history.clear()
-            self.step_number = 0
-        else:
-            rospy.logwarn('Nothing to save')
-
         self.start_tar_pos = self._sample_target()
         self.start_obj_pos = self._sample_object()
 
@@ -181,8 +166,6 @@ class FakeEnv(gym.Env):
     def step(self, action: np.array) -> Tuple[Dict[str, np.array], float, bool, bool, Dict[str, Any]]:
         self.last_action = action
         self.step_number += 1
-        print('  ' + str(self.step_number))
-        if self.debug_mode: self._print_action(action)
 
         # Settaggio dei nuovi parametri attraverso la action.
         # Se l'azione è il nuovo set di parametri
@@ -203,20 +186,6 @@ class FakeEnv(gym.Env):
         terminated = bool(self._is_success())
         truncated = False
         info = {"is_success": terminated}
-
-        # save data
-        if self.param_history:
-            data = []
-            data.append(self.all_param_names + ['reward','iteration'])
-            for index in range(len(self.param_history)):
-                data.append(self.param_history[index])
-            param_history_ods = od.get_data(self.package_path + "/data/" + self.data_file_name)
-            param_history_ods.update({str(self.epoch_number): data})
-            od.save_data(self.package_path + "/data/" + self.data_file_name, param_history_ods)
-        else:
-            rospy.logwarn('Nothing to save')
-
-
 
         return observation, reward, terminated, truncated, info
 
@@ -246,52 +215,17 @@ class FakeEnv(gym.Env):
         #     print('Reward: ' + str(reward))
 
         reward = 1
-        reward -= self._distance(self.current_grasp_pos,self.relative_correct_grasp_pos)
-        reward -= self._distance(self.current_insert_pos,self.relative_correct_insert_pos)
+        reward -= self._distance(self.current_grasp_pos,self.relative_correct_grasp_pos) * 4
+        reward -= self._distance(self.current_insert_pos,self.relative_correct_insert_pos) * 4
 
-        # Qua riempio lo storico dei parametri e il relativo reward
-        rew_step_n = np.array([reward,self.step_number]).tolist()
-        param_values = self.param_values.tolist()
-        last_action = self.last_action.tolist()
-        self.param_history.append(param_values + rew_step_n + last_action)
-
-        print('current_grasp_pos ' + str(self.current_grasp_pos))
-        print('current_insert_pos ' + str(self.current_insert_pos))
-        print('correct_grasp_pos ' + str(self.relative_correct_grasp_pos))
-        print('correct_insert_pos ' + str(self.relative_correct_insert_pos))
-        print('grasp distance ' + str(self._distance(self.current_grasp_pos,self.relative_correct_grasp_pos)))
-        print('insert distance ' + str(self._distance(self.current_insert_pos,self.relative_correct_insert_pos)))
-        print('reward')
+        # print('action ' + str(self.last_action))
+        # print('param ' + str(self.param_values))
+        # print('current_grasp_pos ' + str(self.current_grasp_pos))
+        # print('current_insert_pos ' + str(self.current_insert_pos))
+        # print('correct_grasp_pos ' + str(self.relative_correct_grasp_pos))
+        # print('correct_insert_pos ' + str(self.relative_correct_insert_pos))
+        # print('grasp distance ' + str(self._distance(self.current_grasp_pos,self.relative_correct_grasp_pos)))
+        # print('insert distance ' + str(self._distance(self.current_insert_pos,self.relative_correct_insert_pos)))
+        # print('reward')
 
         return reward
-
-    def _print_action(self, action) -> None:
-        print(' ')
-        print('ACTION____________________________________________________________________')
-        for param_name in self.param_names_to_value_index.keys():
-            if len(self.param_names_to_value_index[param_name]) == 1:
-                print(param_name + ': ' + str(action[self.param_names_to_value_index[param_name][0]]))
-            else:
-                param_len = len(self.param_names_to_value_index[param_name])
-                start_index = self.param_names_to_value_index[param_name][0]
-                print(param_name + ': ' + str(action[start_index:start_index+param_len]))
-        print('--------------------------------------------------------------------------')
-        print(' ')
-
-    def _print_obs(self, observation) -> None:
-        print(' ')
-        print('OBSERVATION_______________________________________________________________')
-        for param_name in self.param_names_to_value_index.keys():
-            if len(self.param_names_to_value_index[param_name]) == 1:
-                print(param_name + ': ' + str(observation[self.param_names_to_value_index[param_name][0]]))
-            else:
-                param_len = len(self.param_names_to_value_index[param_name])
-                start_index = self.param_names_to_value_index[param_name][0]
-                print(param_name + ': ' + str(observation[start_index:start_index+param_len]))
-
-        start_index = len(self.param_values)
-        print('obj_to_grasp_pos: ' + str(observation[start_index:start_index+3]))
-        print('tar_to_insertion_pos: ' + str(observation[start_index+3:start_index+6]))
-        # print('max_wrench: ' + str(observation[start_index+6:start_index+12]))
-        print('--------------------------------------------------------------------------')
-        print(' ')
--- birmingham_cell_tests/src/fake_train.py
+++ birmingham_cell_tests/src/fake_train.py
@@ -5,9 +5,8 @@ import numpy as np
 import birmingham_envs
 
 from stable_baselines3 import TD3
-# from stable_baselines3.common.noise import NormalActionNoise
+from stable_baselines3.common.noise import NormalActionNoise
 from stable_baselines3.common.callbacks import CheckpointCallback
-from stable_baselines3.common.env_util import make_vec_env
 
 import rospkg
 import os
@@ -48,14 +47,14 @@ class MyCheckpointCallback(CheckpointCallback):
 
 
 test_name = "fake_1_"
-epoch_number = 500
-max_epoch_steps = 60
-learning_start_steps = 40
+epoch_number = 1000
+max_epoch_steps = 200
+learning_start_steps = 1000
 train_freq = 1
-learning_rate = 0.001
-gamma = 0.9
+learning_rate = 0.0001
+gamma = 0.5
 total_timesteps = max_epoch_steps * epoch_number
-model_save_freq = 1
+model_save_freq = 60
 
 rospack = rospkg.RosPack()
 path = rospack.get_path('birmingham_cell_tests')
@@ -75,34 +74,25 @@ models_repo_path = model_repo_path + '/' + models_name
 env = gym.make('FakeEnv-v0',
                action_type='increment_value', 
                max_episode_steps=max_epoch_steps, 
-               data_file_name=data_name,
-            #    debug_mode=True)
-               debug_mode=False)
+               data_file_name=data_name)
 
-# env = gym.NormalizeObservation(env)
-
-#checkpoint_callback = CheckpointCallback(
-#    save_freq=max_epoch_steps,
-#    save_path=models_repo_path + '/',
-#    name_prefix=model_name,
-#    keep_only_best=False
-#)
 checkpoint_callback = MyCheckpointCallback(save_freq=model_save_freq,
                                            save_path=models_repo_path + '/', 
                                            name_prefix=model_name)  
 
-# n_actions = env.action_space.shape[-1]
-# action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+n_actions = env.action_space.shape[-1]
+action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
 
 model = TD3("MlpPolicy", 
             env, 
             verbose=1, 
-            # action_noise=action_noise,
+            action_noise=action_noise,
             learning_rate=learning_rate,
-            learning_starts=learning_start_steps, 
+            # learning_starts=learning_start_steps, 
             tensorboard_log=log_path,
-            train_freq=train_freq,
-            gamma=gamma)
+            # train_freq=train_freq,
+            gamma=gamma,
+            )
 
 model.learn(total_timesteps=total_timesteps, log_interval=1, callback=checkpoint_callback)
 
